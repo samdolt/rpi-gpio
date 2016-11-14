37ece756adafa7678fd25e3c87cbd3eadfe68b9b
Samuel Dolt <samuel@dolt.ch>
2016-11-14 21:58:37 +0100

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate peripheral_linux;
+extern crate peripheral;
+use peripheral::prelude::*;
+
+use peripheral_linux::GPIO;
+
+use std::thread::sleep;
+use std::time::Duration;
+
+fn wait() {
+        sleep(Duration::from_millis(1000));
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut gpio = GPIO::new(26).unwrap();
+
+    loop {
+        gpio.set_high().unwrap();
+        wait();
+        gpio.set_low().unwrap();
+        wait();
+    }
 }
