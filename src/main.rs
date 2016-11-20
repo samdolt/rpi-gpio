d618f8d4cf2396004e9514c53b9d3a8c3b3e2dfb
Samuel Dolt <samuel@dolt.ch>
2016-11-20 20:18:45 +0100

    Add spi

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
 extern crate peripheral_linux;
 extern crate peripheral;
+extern crate spidev;
+
 use peripheral::prelude::*;
 
 use peripheral_linux::GPIO;
 
 use std::thread::sleep;
 use std::time::Duration;
+use std::io::Write;
+
+use spidev::{Spidev, SpidevOptions, SpidevTransfer, SPI_MODE_0};
 
 fn wait() {
-        sleep(Duration::from_millis(1000));
+    sleep(Duration::from_millis(1000));
 }
 
 fn main() {
 
-    let mut gpio = GPIO::new(26).unwrap();
+    let mut out_en = GPIO::new(22).unwrap();
+
+    let mut spi = Spidev::open("/dev/spidev0.1").unwrap();
+    let mut options = SpidevOptions::new();
+    options.bits_per_word(8);
+    options.max_speed_hz(20_000);
+    options.mode(SPI_MODE_0);
+    spi.configure(&options).unwrap();
+
+    out_en.set_to_output().unwrap();
+    out_en.set_high().unwrap();
+
+    spi.write(&[0xF0]);
 
     loop {
-        gpio.set_to_output().unwrap();
-        gpio.set_high().unwrap();
-        wait();
-        gpio.set_low().unwrap();
-        wait();
     }
 }
